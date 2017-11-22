21468fba25db6d2eaa21f5be9d05736b2cce5a79
Vim L <lee.rinrire@gmail.com>
2017-11-22 21:55:47 +0100

    fetchAppDetails can write data into a .json
    To write the retrieved data into a .json file,
    set the boolean 'write' to True (default value is False).
    The output file's name is as follows: 'appid.json'.
    E.g fetchAppDetails(730, True) will output '730.json'.

--- src/fetchdata.py
+++ src/fetchdata.py
@@ -1,7 +1,7 @@
 import urllib.request, json
 from pprint import pprint
 
-def fetchAppDetails(appid):
+def fetchAppDetails(appid, write=False):
     """Returns the fetched data from Steam API.
     Steam API's format is: https://api.steampowered.com/<interface>/<method>/v<version>/
     In this case, it should be: http://store.steampowered.com/api/appdetails/?appids=VALUE&format=json
@@ -23,4 +23,11 @@ def fetchAppDetails(appid):
     request = host + "/" + interface + "/" + method + "/" + get + str(appid) + "&" + format + file_type
     with urllib.request.urlopen(request) as url:
         data = json.loads(url.read().decode())
+        success = data[str(appid)]['success']
+        if write:
+            if success:
+                f = open(str(appid) + '.json', 'w')
+                json.dump(data, f)
+            else:
+                print('AppID \'' + str(appid) + '\' doesn\'t exist.')
         return data
