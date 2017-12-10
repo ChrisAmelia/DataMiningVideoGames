d98ad52a45aef481d0503ac98e286b5df54e77ae
Vim L <lee.rinrire@gmail.com>
2017-12-10 18:50:04 +0100

    Cluster data
    Not finished

--- src/clustering.py
+++ src/clustering.py
@@ -3,6 +3,11 @@ import json, os
 from pprint import pprint
 from utilities import cleanHTML
 
+# Clustering features
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+
 def vectorizeData():
     """Returns a list containing the description of each game present in 'filtered_data.json'.
     Returns:
@@ -31,3 +36,33 @@ def vectorizeData():
     finally:
         os.chdir(curdir)
     return lst
+
+def cluster_data(lst, display_clusters_key = False):
+    """TODO: Docstring for cluster_data.
+
+    Source: https://stackoverflow.com/questions/27889873/clustering-text-documents-using-scikit-learn-kmeans-in-python
+    :lst: TODO
+    :returns: TODO
+
+    """
+    documents = lst
+    vectorizer = TfidfVectorizer(stop_words = 'english')
+    X = vectorizer.fit_transform(documents)
+
+    # Cluster documents
+    true_k = 10
+    model = KMeans(n_clusters = true_k, init = 'k-means++', max_iter = 100, n_init = 1)
+    model.fit(X)
+
+    if display_clusters_key:
+        # Print top terms per cluster clusters
+        print("Top terms per cluster:")
+        order_centrois = model.cluster_centers_.argsort()[:, ::-1]
+        terms = vectorizer.get_feature_names()
+        for i in range(true_k):
+            print("Cluster {:d}".format(i))
+            for ind in order_centrois[i, :10]:
+                print(" {:s}".format(terms[ind]))
+            print()
+
+cluster_data(vectorizeData(), display_clusters_key = True)
